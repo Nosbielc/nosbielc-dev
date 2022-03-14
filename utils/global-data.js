c383710a363ed45dcb29f4c11a57998de1aadbef
Cleibson Gomes da Silva <cleibson.gomes.silva@gmail.com>
2022-03-14 17:05:52 -0300

    First Commit
    First Commit

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Cleibson Gomes';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Code And Coffee';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Made with ❤️ in Quebec, CA.';
 
   return {
     name,
